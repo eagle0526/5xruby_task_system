e16408891481850b5aa551c01af316e5bd54ec88
Yee <a034506618@gmail.com>
2023-02-21 01:05:04 +0800

    I18n setting

--- spec/features/task_spec.rb
+++ spec/features/task_spec.rb
@@ -70,61 +70,6 @@ feature "Task", type: :feature do
       expect(page).to_not have_content(task1.content)
     end
 
-
-
-#   scenario "Task CURD" do    
-#     # step 1 - index
-#     visit tasks_path    
-#     expect(page).to have_content("任務列表")
-
-#     # step 2 - new & create
-#     click_link "新增任務" 
-#     fill_in "task_title", with: "新任務"
-#     fill_in "task_content", with: "我是新任務我是新任務我是新任務我是新任務我是新任務"
-#     select "High", from: "task_priority"
-#     select "Pending", from: "task_state"
-#     fill_in "task_classification", with: "後端工程師"
-#     fill_in "task_start_time", with: "2023-02-23 11:00"
-#     fill_in "task_end_time", with: "2023-02-24 15:00"
-#     # select "yeee", from: "task_user_id"    
-    
-
-#     user = User.create(name: "user1", email: "test@gmail.com", password: "111aaa")
-#     fill_in "task_user_id", with: "#{user.id}"
-    
-    
-#     click_on "新增任務"     
-#     # 確認是否有存進去
-#     task = Task.last
-#     expect(task.title).to eq("新任務")
-        
-#     # 並確認是否有新增任務成功
-#     expect(page).to have_content("新任務")
-    
-
-#     # step 3 - read
-#     click_on "#{task.id}"
-#     expect(page).to have_content("任務詳細資料")
-#     click_on "回上一頁"
-#     expect(page).to have_content("任務列表")
-
-#     #step 4 - edit & update
-#     click_on "編輯"
-#     fill_in "task_title", with: "新任務 - 改"
-#     fill_in "task_content", with: "1231232"
-#     click_on "新增任務" 
-
-    # p "-" * 50
-    # p task
-    # p "-" * 50
-          
-    # expect(task.title).to eq("新任務 - 改")
-        
-    # # 並確認是否有新增任務成功
-    # expect(page).to have_content("新任務 - 改")
-
-    # save_and_open_page
-#   end
 end
 
     # p "-" * 50
