9a88dfea6f922802a2c6437de7828d5c266523a7
Yee <a034506618@gmail.com>
2023-03-06 01:43:39 +0800

    task state setting and model/feature test update

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,6 +9,16 @@ class TasksController < ApplicationController
     @query = @task_page.ransack(params[:q])
     @tasks = @query.result(distinct: true)
 
+    @tasks.each do |task|
+      if Time.now >= task.start_time
+        task.start! if task.pending?
+      end
+  
+      if Time.now >= task.end_time
+        task.end! if task.doing?
+      end
+    end
+
   end
 
   def new    
@@ -17,18 +27,24 @@ class TasksController < ApplicationController
   end
 
 
-  def create
-    # render html: params
+  def create    
     @task = current_user.tasks.new(params_task)  
 
     if @task.save      
-      redirect_to root_path, notice: "成功新增任務"
+      redirect_to tasks_path, notice: "成功新增任務"
     else
       render :new      
     end
   end
 
   def show
+    if Time.now >= @task.start_time
+      @task.start! if @task.pending?
+    end
+
+    if Time.now >= @task.end_time
+      task.ending! if task.doing?
+    end
   end
 
   def edit
--- spec/models/task.rb
+++ spec/models/task.rb
@@ -4,6 +4,9 @@ require 'rails_helper'
 
 RSpec.describe Task, :type => :model do
 
+  let!(:user) { create(:user) }
+  let!(:task) { create(:task, user: user) }
+
   it { should validate_presence_of(:title).with_message('任務標題不能為空') }
   it { should validate_presence_of(:content).with_message('任務內容不能為空') }
   it { should validate_presence_of(:start_time).with_message('任務開始時間不能為空') }
@@ -14,6 +17,23 @@ RSpec.describe Task, :type => :model do
     it "return correct attributes" do
       attributes = Task.ransackable_attributes
       expect(attributes).to eq(["classification", "content", "state", "title"])      
+    end
+  end
+
+  describe "aasm state transitions" do
+    it 'start with the pending state' do
+      expect(task.pending?).to be true
+    end
+
+    it 'state change from pending to doing' do
+      task.start!
+      expect(task.doing?).to be true
+    end
+
+    it 'state change from doing to ending' do
+      task.start!
+      task.end!
+      expect(task.ending?).to be true
     end
   end
 
